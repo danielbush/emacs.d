da1810b137bd61414e378cb30f38d5566035494f
Daniel Bush <daniel.bush@domain.com.au>
2017-01-04 00:20:53 +1100

    FEATURE: me/npm and removed me/npm/build .

--- extra/node.el
+++ extra/node.el
@@ -4,23 +4,23 @@
 (require 'projectile)
 (require 'me/utils (concat default-directory "utils.el"))
 
+(defun me/npm (command)
+  "Run npm COMMAND and save to a unique buffer."
+  (interactive "snpm: ")
+  (let* ((buffer-name (me/make-command-buffer-name (concat "npm " command))))
+    (async-shell-command (concat "TERM=dumb npm " command) buffer-name)))
+
 (defun me/npm/run (command)
   "Run npm run COMMAND and save to a unique buffer."
   (interactive "snpm run: ")
   (let* ((buffer-name (me/make-command-buffer-name (concat "npm run " command))))
-    (async-shell-command (concat "TERM=xterm npm run " command) buffer-name)))
+    (async-shell-command (concat "TERM=dumb npm run " command) buffer-name)))
 
 (defun me/npm/run/test ()
   "Run npm test and save to a unique buffer."
   (interactive)
   (let* ((buffer-name (me/make-command-buffer-name "npm run test")))
-    (async-shell-command "TERM=xterm npm run test" buffer-name)))
-
-(defun me/npm/run/build ()
-  "Run npm run build and save to a unique buffer."
-  (interactive)
-  (let* ((buffer-name (me/make-command-buffer-name "npm run build")))
-    (async-shell-command "TERM=xterm npm run build" buffer-name)))
+    (async-shell-command "TERM=dumb npm run test | sed -e 's/\\\[0G//g'" buffer-name)))
 
 (provide 'me/node)
 ;;; node.el ends here
