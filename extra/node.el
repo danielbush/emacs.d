3bf4e01d28e4886a96fc40d886382fc8f9249a0e
Daniel Bush <daniel.bush@domain.com.au>
2017-01-07 17:03:33 +1100

    Hack to solve npm control char ^[[0G problem.

--- extra/node.el
+++ extra/node.el
@@ -14,13 +14,41 @@
   "Run npm run COMMAND and save to a unique buffer."
   (interactive "snpm run: ")
   (let* ((buffer-name (me/make-command-buffer-name (concat "npm run " command))))
-    (async-shell-command (concat "TERM=dumb npm run " command) buffer-name)))
+    (async-shell-command (concat "TERM=xterm npm run " command) buffer-name) ))
 
 (defun me/npm/run/test ()
   "Run npm test and save to a unique buffer."
   (interactive)
   (let* ((buffer-name (me/make-command-buffer-name "npm run test")))
-    (async-shell-command "TERM=dumb npm run test | sed -e 's/\\\[0G//g'" buffer-name)))
+    ;(async-shell-command "TERM=xterm npm run test | sed -e 's/\\\[0G//g'" buffer-name)
+    (async-shell-command "TERM=xterm npm run test" buffer-name)
+    ;(start-process "npm run test" buffer-name "npm" "run" "test")
+    (dotimes (i 6)
+      (1+ i) (run-at-time i nil
+                          `(lambda ()
+                             (progn
+                               (message (format "running tidy up with i=%s" ,i))
+                               (me/npm/tidy-up-buffer-escape-chars ,buffer-name)))))
+    ))
+
+(defun waith-for-process-to-finish ()
+  (interactive)
+  (catch 'break
+      (while t
+        (if (get-buffer-process buffer-name)
+            (sleep-for 0.5)
+          (progn (message "here!!!") (throw 'break nil)))))
+)
+
+(defun me/npm/tidy-up-buffer-escape-chars (&optional buffer-name)
+  (interactive)
+  (let ((buffer-nm (if buffer-name buffer-name (buffer-name))))
+    (with-current-buffer buffer-nm
+      (replace-string "[0G" "" nil (point-min) (point-max))
+      (replace-string "" "" nil (point-min) (point-max))
+      )
+    )
+  )
 
 (provide 'me/node)
 ;;; node.el ends here
