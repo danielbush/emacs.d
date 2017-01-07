067375cf17b484a9b964a7c9bbc1f62e4b3105f7
Daniel Bush <daniel.bush@domain.com.au>
2017-01-07 16:02:40 +1100

    Some elscreen configs.

---
+++ extra/elscreen.el
@@ -0,0 +1,48 @@
+(defun me/elscreen-new (name)
+  (interactive "sName: ")
+  (elscreen-create)
+  (elscreen-screen-nickname name)
+  )
+
+(defun me/elscreen-make (name path &optional dont-make)
+  (unless dont-make (elscreen-create))
+  (elscreen-screen-nickname name)
+  (find-file path)
+  (delete-other-windows)
+  )
+
+(defun me/elscreen-start ()
+  (interactive)
+  (elscreen-start)
+  (elscreen-goto 0) ; need this as well as kill-others
+  (elscreen-kill-others)
+)
+
+(defun me/setup-elscreen ()
+  "Preload some elscreens."
+  (interactive)
+  (me/elscreen-start)
+  (me/elscreen-make "1" "~/projects" t)
+  (me/elscreen-make "2" "~/projects")
+  (me/elscreen-make "3" "~/projects")
+  (me/elscreen-make "4" "~/projects")
+)
+
+(defun me/setup-elscreen-alternative-for-domain ()
+  "Preload some elscreens."
+  (interactive)
+  (me/elscreen-start)
+  (me/elscreen-make "1" "~/.emacs.d/me" t)
+  (me/elscreen-make "2" "~/notes")
+  (me/elscreen-make "3" "~/")
+  (me/elscreen-make "4" "~/")
+)
+
+
+
+
+(defun me/elscreen-goto-screen (num)
+  "NUM should be an integer."
+  (interactive)
+  (elscreen-goto num) (message (elscreen-get-screen-nickname num))
+  )
