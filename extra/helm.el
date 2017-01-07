a8ed8c33fb0592a408fd8587de1e3804114f077f
Daniel Bush <dlb.id.au@gmail.com>
2017-01-08 09:33:04 +1100

    FIX todos logs path.

--- extra/helm.el
+++ extra/helm.el
@@ -30,7 +30,7 @@
                           (split-string
                            (shell-command-to-string
                             (concat
-                             "find -L $HOME/logs -maxdepth 3 -printf \"%p\n\" | grep -v '/\.git'"
+                             "find -L $HOME/todos/logs -maxdepth 3 -printf \"%p\n\" | grep -v '/\.git'"
                              ;; "find -L . -type f -path \"" pattern "\" -printf \"%P\n\""
                              ;; "find -L . -type f -name \"" pattern "\" -printf \"%P\n\""
                              ;; "find -L . -type f -name \"" pattern "\""
