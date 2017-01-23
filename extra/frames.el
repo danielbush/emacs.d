32eef17b8e6295030a0b1a8026f1653d8671e04a
Daniel Bush <daniel.bush@domain.com.au>
2017-01-23 17:54:44 +1100

    Stick with 3 frames.

--- extra/frames.el
+++ extra/frames.el
@@ -6,7 +6,7 @@
   (me/make-and-name-frame "1" 'me/bg-black t)
   (me/make-and-name-frame "2" 'me/bg-blue)
   (me/make-and-name-frame "3" 'me/bg-dark)
-  (me/make-and-name-frame "4" 'me/bg-purple)
+  ;(me/make-and-name-frame "4" 'me/bg-purple)
   )
 
 (defun me/make-and-name-frame (name colour-fn &optional dont-make)
