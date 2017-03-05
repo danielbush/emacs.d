c59888f0cd886fd893b33a17037bceb74636bcc5
Daniel Bush <dlb.id.au@gmail.com>
2017-03-05 12:09:05 +1100

    Rjsx mode and change region colour.

--- settings.el
+++ settings.el
@@ -102,7 +102,9 @@
 (require 'helm-projectile)
 
 ; Make js2-mode the default for js.
-(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.js\\'" . rjsx-mode))
+
+(set-face-attribute 'region nil :background "#ee0" :foreground "#000")
 
 
 ;(me/font-medium)
