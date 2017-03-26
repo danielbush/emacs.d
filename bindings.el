834b766c5df3e027284787a3b31dbcb19e7da30b
Daniel Bush <dlb.id.au@gmail.com>
2017-03-26 12:08:33 +1100

    Avy goto word binding.

--- bindings.el
+++ bindings.el
@@ -34,7 +34,7 @@
   (global-set-key (kbd "C-0") '(lambda () (interactive) (me/elscreen-goto-screen 9)))
 
   ;(global-set-key (kbd "M-p") 'avy-goto-word-or-subword-1)
-  (global-set-key (kbd "M-z") 'avy-goto-word-or-subword-1)
+  (global-set-key (kbd "M-c") 'avy-goto-word-or-subword-1)
 
   (global-set-key (kbd "C-M-h") 'windmove-left)
   (global-set-key (kbd "C-M-j") 'windmove-down)
