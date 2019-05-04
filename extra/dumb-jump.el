949897f7dd9d7bba73273eee3615acd93f982eda
Daniel Bush <dlb.id.au@gmail.com>
2019-05-04 10:43:57 +1000

    dumb-jump

--- extra/dumb-jump.el
+++ extra/dumb-jump.el
@@ -1,5 +1,43 @@
+;;; extra/dumb-jump -- extra configs
+;;; Commentary:
+;;; Code:
 (require 'thingatpt)
 (require 'dumb-jump)
+(require 'js2-mode)
+(require 'groovy-mode)
+
+(defun me/dumb-jump-keys (mode-map)
+  (define-key mode-map (kbd "M-.") 'dumb-jump-go)
+  (define-key mode-map (kbd "C-M-.") 'dumb-jump-go-other-window)
+  (define-key mode-map (kbd "M-,") 'dumb-jump-back)
+
+  ;; (define-key mode-map (kbd "M-.") 'me/dumb-jump-go-prompt)
+  ;; (define-key mode-map (kbd "C-M-.") 'me/dumb-jump-go-prompt-other-window)
+  )
+
+(add-hook 'js2-mode-hook (lambda () (me/dumb-jump-keys js2-mode-map)))
+(add-hook 'groovy-mode-hook (lambda () (me/dumb-jump-keys groovy-mode-map)))
+
+;;; --------------------------------------------------------------------------------
+;;; Experiment
+
+;; Jumping on foo.|bar() doesn't behave the same as jumping on foo.|bar in js.
+;; Actually "Dumb Jump Ignore Context" = on will fix this problem.
+;; However, this is another workaround - because prompt doesn't have context either.
+
+(defun me/dumb-jump-go-prompt ()
+  (interactive)
+  (dumb-jump-go nil nil (read-from-minibuffer "Jump to: " (thing-at-point 'symbol))))
+
+(defun me/dumb-jump-go-prompt-other-window ()
+  (interactive)
+  (let ((dumb-jump-window 'other)
+        (input (read-from-minibuffer "Jump to: " (thing-at-point 'symbol))))
+    (dumb-jump-go nil nil input)
+  ))
+
+;;; --------------------------------------------------------------------------------
+;;; Experiment
 
 ;; Jumping on foo.|bar() doesn't behave the same as jumping on foo.|bar in js.
 ;; Actually "Dumb Jump Ignore Context" = on will fix this problem.
--- extra/tern.el
+++ extra/tern.el
@@ -6,7 +6,6 @@
 
 (require 'company)
 (require 'company-tern)
-(require 'extra/dumb-jump)
 
 (add-to-list 'company-backends 'company-tern)
 (add-hook 'js2-mode-hook (lambda ()
@@ -19,15 +18,7 @@
 (define-key tern-mode-keymap (kbd "M-,") nil)
 (define-key tern-mode-keymap (kbd "C-M-.") nil)
 
-(defun me/js2-mode-hook() (me/mode-hook js2-mode-map))
-(defun me/groovy-mode-hook() (me/mode-hook groovy-mode-map))
-
 (defun me/mode-hook(mode-map)
-   (define-key mode-map (kbd "M-.") 'dumb-jump-go)
-   ;; (define-key mode-map (kbd "M-.") 'me/dumb-jump-go-prompt)
-   (define-key mode-map (kbd "C-M-.") 'dumb-jump-go-other-window)
-   ;; (define-key mode-map (kbd "C-M-.") 'me/dumb-jump-go-prompt-other-window)
-   (define-key mode-map (kbd "M-,") 'dumb-jump-back)
 
    (hs-minor-mode 1)
    (define-key mode-map (kbd "C-c C-f") 'hs-hide-level)
@@ -36,12 +27,6 @@
    ;; (define-key mode-map (kbd "C-c C-b") 'hs-show-block)
    ;; (define-key mode-map (kbd "C-c C-h") 'hs-hide-block)
   )
-
-(add-hook
- 'js2-mode-hook 'me/js2-mode-hook)
-
-(add-hook
- 'groovy-mode-hook 'me/groovy-mode-hook)
 
 (provide 'extra/tern)
 ;;; tern.el ends here
